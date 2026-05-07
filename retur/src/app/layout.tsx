22ffc5143984e0f5a332f9e1ac646e62a06820ce
lucasguerra12 <lucasguerrawr2004@gmail.com>
2026-05-07 17:35:10 -0300

    Add Facebook Pixel tracking script and update reviews section with new testimonials

--- retur/src/app/layout.tsx
+++ retur/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 // Importações nomeadas corretas, exatamente como nos seus arquivos de componentes
 import { Header } from "../components/Header";
@@ -33,6 +34,22 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
       <body suppressHydrationWarning className="antialiased font-manrope">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            
+            // Substitua o número abaixo pelo ID que a cliente mandou:
+            fbq('init', 'COLOQUE_O_NUMERO_AQUI'); 
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
